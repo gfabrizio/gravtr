501299281d4d27f1a3007c859a7c793a35d2f39c
Guilherme Fabricio <gfabricio@gmail.com>
2016-02-19 23:11:18 -0200

    added a test to function that generates profile, fix pep8 issues

--- tests.py
+++ tests.py
@@ -1,14 +1,15 @@
 import hashlib
+import re
 import unittest
 
 from gravtr import Gravtr
+
 
 class GravtrTestCase(unittest.TestCase):
     def setUp(self):
         self.email = "gfabricio@gmail.com".encode('utf-8')
         self.gravtr_url = ("http://www.gravatar.com/avatar/" +
-                     hashlib.md5(self.email).hexdigest())
-
+                           hashlib.md5(self.email).hexdigest())
 
     def test_generate_url_should_succeed(self):
         gravtr = Gravtr(email=self.email)
@@ -17,6 +18,11 @@ class GravtrTestCase(unittest.TestCase):
     def test_generate_url_with_specified_size_should_succeed(self):
         gravtr = Gravtr(email=self.email)
         assert self.gravtr_url + '?' + str(200), gravtr.generate(size=200)
+
+    def test_generate_a_valid_profile_url(self):
+        gravtr = Gravtr(email=self.email)
+        profile_url = re.sub('\.avatar/$', '', self.gravtr_url)
+        assert profile_url, gravtr.profile()
 
 
 if __name__ == '__main__':
