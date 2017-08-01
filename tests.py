6a6fba26d3e6b6ff3a9179d1d57e81b216cc4eb0
Denick Murray <denick@denick.org>
2017-08-01 19:09:33 +0200

    Make urllib and string literal prefixes work on both py2 and py3.

--- tests.py
+++ tests.py
@@ -16,26 +16,26 @@ class GravtrTestCase(unittest.TestCase):
 
     def test_generate_url_with_specified_size_should_succeed(self):
         gravtr = Gravtr(email=self.email).generate(size=200)
-        assert re.search(ur"(\?|\&)s\=200", gravtr)
+        assert re.search(r"(\?|\&)s\=200", gravtr)
 
     def test_generate_url_with_type_should_succeed(self):
         gravtr = Gravtr(email=self.email).generate(typed=True, size=200)
-        assert re.search(ur"\.jpg", gravtr)
+        assert re.search(r"\.jpg", gravtr)
 
     def test_generate_url_with_default_should_succeed(self):
         default_image = 'http://example.com/image.jpg'
         gravtr = Gravtr(email=self.email).generate(default=default_image, size=200)
-        assert re.search(ur"(\?|\&)d\=http\%3A\%2F\%2Fexample\.com\%2Fimage\.jpg", gravtr)
+        assert re.search(r"(\?|\&)d\=http\%3A\%2F\%2Fexample\.com\%2Fimage\.jpg", gravtr)
 
     def test_generate_url_with_force_default_should_succeed(self):
         default_image = 'http://example.com/image.jpg'
         gravtr = Gravtr(email=self.email).generate(default=default_image, force_default=True)
-        assert re.search(ur"(\?|\&)d\=http\%3A\%2F\%2Fexample\.com\%2Fimage\.jpg", gravtr)
-        assert re.search(ur"(\?|\&)f\=y", gravtr)
+        assert re.search(r"(\?|\&)d\=http\%3A\%2F\%2Fexample\.com\%2Fimage\.jpg", gravtr)
+        assert re.search(r"(\?|\&)f\=y", gravtr)
 
     def test_generate_url_with_rating_type_should_succeed(self):
         gravtr = Gravtr(email=self.email).generate(typed=True, rating_type=Gravtr.ratingType.X)
-        assert re.search(ur"(\?|\&)r\=x", gravtr)
+        assert re.search(r"(\?|\&)r\=x", gravtr)
 
 if __name__ == '__main__':
     unittest.main()
